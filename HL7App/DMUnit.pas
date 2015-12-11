708955887c013f9ed499091ee5238220e95645f0
preosvan <preosvan@gmail.com>
2015-12-11 02:31:53 +0200

    Added HL7Client

--- HL7App/DMUnit.pas
+++ HL7App/DMUnit.pas
@@ -4,17 +4,19 @@ interface
 
 uses
   System.SysUtils, System.Classes, Data.DbxSqlite, Data.DB, Data.SqlExpr,
-  Data.FMTBcd;
+  Data.FMTBcd, IdHL7;
 
 type
   TDM = class(TDataModule)
     SQLConnection: TSQLConnection;
     SQLQuery: TSQLQuery;
     procedure DataModuleCreate(Sender: TObject);
+    procedure DataModuleDestroy(Sender: TObject);
   private
-    { Private declarations }
+    FHL7Client: TIdHL7;
+    procedure HL7ClientInit;
   public
-    { Public declarations }
+    property HL7Client: TIdHL7 read FHL7Client;
   end;
 
 var
@@ -34,6 +36,25 @@ begin
   SQLConnection.Params.Values['Database'] :=
     ExtractFilePath(ParamStr(0)) + 'HL7_DB.sqlite';
   SQLConnection.Open;
+  HL7ClientInit;
+end;
+
+procedure TDM.DataModuleDestroy(Sender: TObject);
+begin
+  if Assigned(FHL7Client) then
+    FHL7Client.Free;
+end;
+
+procedure TDM.HL7ClientInit;
+begin
+  FHL7Client := TIdHL7.Create(Self);
+  FHL7Client.CommunicationMode := cmSingleThread;
+  FHL7Client.ReceiveTimeout := 4000;
+  FHL7Client.TimeOut := 4000;
+  FHL7Client.ReconnectDelay := 2000;
+  FHL7Client.Address := '';
+  FHL7Client.Port := 21;
+  FHL7Client.Start;
 end;
 
 end.
--- HL7App/HL7App.dpr
+++ HL7App/HL7App.dpr
@@ -6,7 +6,8 @@ uses
   DMUnit in 'DMUnit.pas' {DM: TDataModule},
   MedModel in 'MedModel.pas',
   MedUtils in 'MedUtils.pas',
-  MedModelConst in 'MedModelConst.pas';
+  MedModelConst in 'MedModelConst.pas',
+  IdHL7 in 'IdHL7.pas';
 
 {$R *.res}
 
