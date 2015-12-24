5543c7ff46898e697158e72d30d8de6d73c3effa
preosvan@gmail.com <preosvan@gmail.com>
2015-12-24 11:55:50 +0200

    Added presentation of the results of parsing OBR and OBX

--- HL7App/MainFrm.pas
+++ HL7App/MainFrm.pas
@@ -66,8 +66,58 @@ begin
   begin
     OBR := TOBR.Create(THL7Segment.GetSegmentMsgTextStr(meMsg.Lines, hlsOBR));
     try
-      meParse.Clear;
-      meParse.Lines.Add(OBR.ToString);
+      meParse.Lines.BeginUpdate;
+      try
+        meParse.Clear;
+        meParse.Lines.Add(OBR.ToString);
+        meParse.Lines.Add('==============================');
+        meParse.Lines.Add(OBR.HL7SegmentName);
+        meParse.Lines.Add('==============================');
+        meParse.Lines.Add(OBR.PlacerOrderNumber);
+        meParse.Lines.Add(OBR.FillerOrderNumber);
+        meParse.Lines.Add(OBR.UniversalServiceID);
+        meParse.Lines.Add(OBR.Priority);
+        meParse.Lines.Add(OBR.RequestedDateTime);
+        meParse.Lines.Add(DateTimeToStr(OBR.ObservationDateTime));
+        meParse.Lines.Add(DateTimeToStr(OBR.ObservationEndDateTime));
+        meParse.Lines.Add(OBR.CollectionVolume);
+        meParse.Lines.Add(OBR.CollectorIdentifier);
+        meParse.Lines.Add(OBR.SpecimenActionCode);
+        meParse.Lines.Add(OBR.DangerCode);
+        meParse.Lines.Add(OBR.RelevantClinicalInfo);
+        meParse.Lines.Add(OBR.SpecimenReceivedDateTime);
+        meParse.Lines.Add(OBR.SpecimenSource);
+        meParse.Lines.Add(OBR.OrderingProvider);
+        meParse.Lines.Add(OBR.OrderCallbackPhoneNumber);
+        meParse.Lines.Add(OBR.PlacerField1);
+        meParse.Lines.Add(OBR.PlacerField2);
+        meParse.Lines.Add(OBR.FillerField1);
+        meParse.Lines.Add(OBR.FillerField2);
+        meParse.Lines.Add(OBR.ResultsRptStatusChngDateTime);
+        meParse.Lines.Add(OBR.ChargeToPractice);
+        meParse.Lines.Add(OBR.DiagnosticServSectID);
+        meParse.Lines.Add(OBR.ResultStatus);
+        meParse.Lines.Add(OBR.ParentResult);
+        meParse.Lines.Add(OBR.QuantityOrTiming);
+        meParse.Lines.Add(OBR.ResultCopiesTo);
+        meParse.Lines.Add(OBR.Parent);
+        meParse.Lines.Add(OBR.TransportationMode);
+        meParse.Lines.Add(OBR.ReasonForStudy);
+        meParse.Lines.Add(OBR.PrincipalResultInterpreter);
+        meParse.Lines.Add(OBR.AssistantResultInterpreter);
+        meParse.Lines.Add(OBR.Technician);
+        meParse.Lines.Add(OBR.Transcriptionist);
+        meParse.Lines.Add(OBR.ScheduledDateTime);
+        meParse.Lines.Add(OBR.NumberOfSampleContainers.ToString);
+        meParse.Lines.Add(OBR.TransportLogisticsOfCollectedSample);
+        meParse.Lines.Add(OBR.CollectorsComment);
+        meParse.Lines.Add(OBR.TransportArrangementResponsibility);
+        meParse.Lines.Add(OBR.TransportArranged);
+        meParse.Lines.Add(OBR.EscortRequired);
+        meParse.Lines.Add(OBR.PlannedPatientTransportComment);
+      finally
+        meParse.Lines.EndUpdate;
+      end;
     finally
       OBR.Free;
     end;
@@ -76,16 +126,35 @@ end;
 
 procedure TMainForm.btnOBXClick(Sender: TObject);
 var
-  OBX: TOBX;
+  HL7Message: THL7Message;
+  I: Integer;
+  Spec: TSpeciman;
 begin
-  if CheckHL7(meMsg.Lines, hlsOBX) then
+  if CheckHL7(meMsg.Lines, hlsNone) then
   begin
-    OBX := TOBX.Create(meMsg.Lines[3]);
+    HL7Message := THL7Message.Create(meMSG.Lines);
     try
-      meParse.Clear;
-      meParse.Lines.Add(OBX.ToString);
+      meParse.Lines.BeginUpdate;
+      try
+        meParse.Clear;
+        for I := 0 to HL7Message.SpecimanList.Count - 1 do
+        begin
+          Spec := TSpeciman(HL7Message.SpecimanList[I]);
+          if Assigned(Spec) then
+          begin
+            meParse.Lines.Add('Speciman ¹' + Spec.Id);
+            meParse.Lines.Add('SpecimenLabel: ' + Spec.SpecimenLabel);
+            meParse.Lines.Add('MicroscopicDesc: ' + Spec.MicroscopicDesc);
+            meParse.Lines.Add('GrossDescription: ' + Spec.GrossDescription);
+            meParse.Lines.Add('Diagnosis: ' + Spec.Diagnosis);
+            meParse.Lines.Add('==============================');
+          end;
+        end;
+      finally
+        meParse.Lines.EndUpdate;
+      end;
     finally
-      OBX.Free;
+      HL7Message.Free;
     end;
   end;
 end;
@@ -98,41 +167,46 @@ begin
   begin
     Patient := TPatient.Create(THL7Segment.GetSegmentMsgTextStr(meMsg.Lines, hlsPID));
     try
-      meParse.Lines.Clear;
-      meParse.Lines.Add(Patient.ToString);
-      meParse.Lines.Add('==============================');
-      meParse.Lines.Add(Patient.HL7SegmentName);
-      meParse.Lines.Add('==============================');
-      meParse.Lines.Add(Patient.PatientID.ToString);
-      meParse.Lines.Add(Patient.PatientIDExt);
-      meParse.Lines.Add(Patient.PatientIDInt);
-      meParse.Lines.Add(Patient.PatientIDAlt);
-      meParse.Lines.Add('==============================');
-      meParse.Lines.Add(Patient.PatientName);
-      meParse.Lines.Add(Patient.Surname);
-      meParse.Lines.Add(Patient.Firstname);
-      meParse.Lines.Add(Patient.Initials);
-      meParse.Lines.Add('==============================');
-      meParse.Lines.Add(Patient.MothMaidenName);
-      meParse.Lines.Add(DateTimeToStr(Patient.DTBirth));
-      meParse.Lines.Add(Patient.Gender);
-      meParse.Lines.Add(Patient.PatientAlias);
-      meParse.Lines.Add(Patient.Race);
-      meParse.Lines.Add('==============================');
-      meParse.Lines.Add(Patient.Address);
-      meParse.Lines.Add(Patient.Address1);
-      meParse.Lines.Add(Patient.Address2);
-      meParse.Lines.Add(Patient.City);
-      meParse.Lines.Add(Patient.ProvinceCode);
-      meParse.Lines.Add(Patient.PostalCode);
-      meParse.Lines.Add(Patient.SEERCountryGeocode);
-      meParse.Lines.Add('==============================');
-      meParse.Lines.Add(Patient.PhoneNumbHome);
-      meParse.Lines.Add(Patient.PhoneNumbBusiness);
-      meParse.Lines.Add(Patient.PrimaryLanguage);
-      meParse.Lines.Add(Patient.MaritalStatus);
-      meParse.Lines.Add(Patient.Religion);
-      meParse.Lines.Add(Patient.DriverLicNumb);
+      meParse.Lines.BeginUpdate;
+      try
+        meParse.Lines.Clear;
+        meParse.Lines.Add(Patient.ToString);
+        meParse.Lines.Add('==============================');
+        meParse.Lines.Add(Patient.HL7SegmentName);
+        meParse.Lines.Add('==============================');
+        meParse.Lines.Add(Patient.PatientID.ToString);
+        meParse.Lines.Add(Patient.PatientIDExt);
+        meParse.Lines.Add(Patient.PatientIDInt);
+        meParse.Lines.Add(Patient.PatientIDAlt);
+        meParse.Lines.Add('==============================');
+        meParse.Lines.Add(Patient.PatientName);
+        meParse.Lines.Add(Patient.Surname);
+        meParse.Lines.Add(Patient.Firstname);
+        meParse.Lines.Add(Patient.Initials);
+        meParse.Lines.Add('==============================');
+        meParse.Lines.Add(Patient.MothMaidenName);
+        meParse.Lines.Add(DateTimeToStr(Patient.DTBirth));
+        meParse.Lines.Add(Patient.Gender);
+        meParse.Lines.Add(Patient.PatientAlias);
+        meParse.Lines.Add(Patient.Race);
+        meParse.Lines.Add('==============================');
+        meParse.Lines.Add(Patient.Address);
+        meParse.Lines.Add(Patient.Address1);
+        meParse.Lines.Add(Patient.Address2);
+        meParse.Lines.Add(Patient.City);
+        meParse.Lines.Add(Patient.ProvinceCode);
+        meParse.Lines.Add(Patient.PostalCode);
+        meParse.Lines.Add(Patient.SEERCountryGeocode);
+        meParse.Lines.Add('==============================');
+        meParse.Lines.Add(Patient.PhoneNumbHome);
+        meParse.Lines.Add(Patient.PhoneNumbBusiness);
+        meParse.Lines.Add(Patient.PrimaryLanguage);
+        meParse.Lines.Add(Patient.MaritalStatus);
+        meParse.Lines.Add(Patient.Religion);
+        meParse.Lines.Add(Patient.DriverLicNumb);
+      finally
+        meParse.Lines.EndUpdate;
+      end;
     finally
       Patient.Free;
     end;
